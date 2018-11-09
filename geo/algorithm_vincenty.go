fad3d5f58b0f3fde24d35c96c7e1bf81554a6d7e
Mats Becker <mats.becker@gmail.com>
2018-11-09 21:30:47 +0100

    Updated readme with examples and added example to main

--- geo/algorithm_vincenty.go
+++ geo/algorithm_vincenty.go
@@ -42,7 +42,6 @@ func (v *Vincenty) CustomMovingPoints(gpxPoint *GPXPoint, previousGPXPoint *GPXP
 
 	// speed < 1 m/s
 	if gpxPoint.Speed < 1.0 {
-		fmt.Println("???")
 		return errors.New("Point Speed below threshold")
 	}
 	gpxPoint.Point.SetPointData(previousGPXPoint.Point, algorithm)
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -123,9 +124,62 @@ func parseFiles(fileDirectory string, files []os.FileInfo, alg geo.Algorithm, ta
 	tableData[21] = append(tableData[21], fmt.Sprintf("%f", segmentStoppedDistance/1000.0)) // Segment  Stopped Distance
 	tableData[22] = append(tableData[22], "------")
 	tableData[23] = append(tableData[23], fmt.Sprintf("%s", elapsed))
+
 }
 
+type CustomAlgorithm struct {
+	CustomParameter float64
+}
+
+// ShouldStandardDeviation (CustomAlgorithm) returns if the standard deviation should be used or not
+func (c *CustomAlgorithm) ShouldStandardDeviation() bool {
+	return false
+}
+
+// Sigma (CustomAlgorithm) returns the sigma for the standard deviation; not used because of 'ShouldStandardDeviation' returns 'false'
+func (c *CustomAlgorithm) Sigma() float64 {
+	return 0
+}
+
+// Duration (CustomAlgorithm) returns the time.Duration from point p1 to previousPoint in sec
+func (c *CustomAlgorithm) Duration(p1 *geo.Point, previousPoint *geo.Point) (float64, error) {
+	return p1.Timestamp.Sub(previousPoint.Timestamp).Seconds(), nil
+}
+
+// CustomMovingPoints (CustomAlgorithm) defines which points should be used for "Moving"Time/Distance and if the it's set the new gpxPoint.Point Data
+func (c *CustomAlgorithm) CustomMovingPoints(gpxPoint *geo.GPXPoint, previousGPXPoint *geo.GPXPoint, algorithm geo.Algorithm) error {
+
+	/* 	Define which points should be used; if a point should be used for calculation then set it's new values like Duration, Distance, Speed, etc.
+	Here we use the set the new value for the points which used for "Moving"Time/Distanc
+	*/
+
+	// speed < 100 m/s
+	if gpxPoint.Speed < 100.0 {
+		return errors.New("Point Speed below threshold")
+	}
+	gpxPoint.Point.SetPointData(previousGPXPoint.Point, algorithm)
+	return nil
+}
+
+// Distance (CustomAlgorithm) returns just 100 as an example
+func (c *CustomAlgorithm) Distance(p1 *geo.Point, previousPoint *geo.Point) (float64, error) {
+	return 101, nil
+}
+
+// Speed (CustomAlgorithm) returns the speed in m/s
+func (c *CustomAlgorithm) Speed(distance float64, duration float64) (float64, error) {
+	return 101.9, nil
+}
+
+// Pace (CustomAlgorithm) returns the pace in s/m
+func (c *CustomAlgorithm) Pace(distance float64, duration float64) (float64, error) {
+	return 20.9, nil
+}
 func main() {
+
+	customAlgorithm := CustomAlgorithm{
+		CustomParameter: 100.9,
+	}
 
 	vincentyWithoutStandardDeviation := geo.Vincenty{
 		ShouldStandardDeviationBeUsed: false,
@@ -196,6 +250,10 @@ func main() {
 	}
 
 	algorithms := []AlgStruct{
+		AlgStruct{
+			Name: "Custom",
+			Alg:  &customAlgorithm,
+		},
 		AlgStruct{
 			Name: "Vincenty w/o Standard Deviation",
 			Alg:  &vincentyWithoutStandardDeviation,
